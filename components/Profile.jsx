86197ecda3a7868c29c6bb0e42f56fe38f7f34f3
Bammy <apple@Ayobami-MacBook-Pro.local>
2023-06-04 12:55:57 +0100

    full project commit

---
+++ components/Profile.jsx
@@ -0,0 +1,28 @@
+import PromptCard from "./PromptCard"
+
+const Profile = ({name, desc, data, handleEdit, handleDelete}) => {
+
+  return (
+    <section className="w-full">
+        <h1 className="head_text text-left">
+          <sspan className='blue_gradient'>{name} Profile</sspan>
+        </h1>
+        <p className="desc text-left">
+          {desc}
+        </p>
+
+        <div className='mt-5 prompt_layout'>
+          {data.map((post) => (
+            <PromptCard 
+              key={post._id}
+              post={post}
+              handleEdit={() => handleEdit && handleEdit(post)}
+              handleDelete={() => handleEdit && handleDelete(post)}
+            />
+          ))}
+      </div>
+    </section>
+  )
+}
+
+export default Profile
